25d1a8eca4be60b8870ace77f377522360724a8c
Poorna <poorna@Poornas-MacBook-Air.local>
2026-04-13 18:48:46 +0530

    Revert "Fix: auto-activate PWA updates to avoid stale admin UI"
    This reverts commit 9809064674d0ca474f13991762ca01edb5c88b6f.

--- public/technician-sw.js
+++ public/technician-sw.js
@@ -23,12 +23,6 @@ self.addEventListener('install', (event) => {
   // Don't call skipWaiting() to prevent automatic refresh
 });
 
-self.addEventListener('message', (event) => {
-  if (event?.data?.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keys) =>
